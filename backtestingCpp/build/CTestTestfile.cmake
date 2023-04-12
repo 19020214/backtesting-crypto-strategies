2d20bbb0193ea48cf9209db28be1f028dba73fc9
Quoc Anh Tran <64301034+19020214@users.noreply.github.com>
2023-04-12 13:52:02 +0700

    backtestingCpp

---
+++ backtestingCpp/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Admin123/PycharmProjects/backtesting-crypto-strategies/backtestingCpp
+# Build directory: C:/Users/Admin123/PycharmProjects/backtesting-crypto-strategies/backtestingCpp/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
